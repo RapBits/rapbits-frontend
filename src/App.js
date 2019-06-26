404edbe0a9c5b813e3188ddc93c95de6773f43fa
arasdean <arasdean@gmail.com>
2019-06-25 18:43:54 -0700

    Adds image tags, and cleans hero section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import { Columns } from "react-bulma-components/full";
 // import Header from './header';
 import './App.css';
 
@@ -15,26 +16,36 @@ class App extends Component {
       <div className="App">
         <div className='header'> 
         <div className='logo_container'> 
-          <div className='logo'> <h2> RAPBITS </h2> </div> 
-          <div className='nav_menu'>
-            <ul class="menu_items">
-              <li> Songs </li> 
-              <li> About </li> 
-              <li> Contact </li> 
-            </ul>  
-          </div>
+          <div className='logo'> <h1>RAPBITS</h1> </div> 
         </div>
         </div> 
         <div className='hero_section'> 
           <div className='main_container'> 
-            <h1> CONTENT </h1> 
             <div className='search_container'> <h2> SEARCH </h2> </div> 
             <div className='tagline_container'> 
               <h2>Rapbits lets you share your favorite rap lines with friends. </h2> 
-              <button className='call_to_action'> Learn More + </button> 
             </div>
           </div>
+        </div>
+        <div className='songs_section'> 
+        <div className='songs_container'> 
+          <div className='img_grid'> 
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+            <img src='https://images.genius.com/d28ff3b1c3466b0234599b62e5857bb0.1000x1000x1.jpg' />
+          </div> 
         </div> 
+        <div className='button_container'> 
+          <button className='call_to_action'> Load More + </button>  
+        </div>
+        </div>
       </div>
     );
   }
